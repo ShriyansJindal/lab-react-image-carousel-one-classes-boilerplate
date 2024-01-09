53d7c5866d5aff714a408e79839175b96b6768fc
shriyans jindal <shriyansjindal40517@gmail.com>
2024-01-09 15:56:53 +0530

    done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,6 +7,61 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
+
+    constructor(){
+        super()
+        this.state={
+            index:0
+        }
+    }
+
+    render(){
+        let {index} = this.state;
+        let currentImage = images[index];
+        let handlePrev=()=>{
+                // this.setState({index:(this.state.index-1+images.length)%images.length})
+
+                if(index==0){
+                    this.setState({index:(2)})
+                }
+                else{
+                    this.setState({index:(this.state.index-1)})
+                }
+        }
+        let handleNext=()=>{
+                // this.setState({index:(this.state.index+1)%images.length})
+                if(index==2){
+                    this.setState({index:(0)})
+                }
+                else{
+                    this.setState({index:(this.state.index+1)})
+                }
+            
+        }
+        return(
+            <>
+            <div className="main">
+                <div >
+                <ArrowBackIosIcon className="arrow" onClick = {handlePrev}/>
+                </div>
+                <div className="imagebox">
+                    <img src={currentImage.img} alt="" />
+                </div>
+                <div className="imageContainer">
+                    <div className="titlebox">
+                        <p>{currentImage.title}</p>
+                    </div>
+                    <div className="subtittle">
+                        <p>{currentImage.subtitle}</p>
+                    </div>
+                </div>
+                <div >
+                    <ArrowForwardIosIcon className="arrow" onClick = {handleNext}/>
+                </div>
+            </div>
+            </>
+        )
+    }
   
 }
 
